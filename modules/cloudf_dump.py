67270339b3a34231ba4eb030aa8e1d7adcf44eb8
Anouar Ben Saad <bensaad.tig@gmail.com>
2022-03-19 15:36:45 +0100

    ADD SCANNER CLOUDFLARE

--- modules/cloudf_dump.py
+++ modules/cloudf_dump.py
@@ -6,17 +6,28 @@ from regex import CF_PARSE_SUB_AND_DOMAIN,CF_IP
 from gen_proxies import proxies_chain
 end = '\033[1;0m'
 red = '\033[1;91m'
-
+green = '\033[1;92m'
 f = open('../db/vulners.json')
 DB_LOAD = json.load(f)
 URL = DB_LOAD["base"]["cloudFlare"]["url"]+DB_LOAD["base"]["cloudFlare"]["endpoint"]
-
+SCANNER = DB_LOAD["base"]["cloudFlare"]["url"]+DB_LOAD["base"]["cloudFlare"]["scanner"]
 def parse_sub_and_domain(data):
     matched = re.findall(CF_PARSE_SUB_AND_DOMAIN,data)
     try:
         return matched
     except Exception as err:
         return None
+
+def wide_scan(target,ip,proxy):
+    params = {
+        "resource": "hosts",
+        "sort": "RELEVANCE",
+        "per_page": "25",
+        "virtual_hosts":"EXCLUDE",
+        "q": target
+    }
+    res = requests.get(SCANNER+ip,params=params,proxies=proxy)
+    return res.text
 
 def parse_ip(data):
     matched = re.findall(CF_IP,data)
@@ -27,8 +38,12 @@ def parse_ip(data):
 
 def cloud_grap(url,target):
     success = False
-    while not success:
-        for p in proxies_chain():
+    temp_proxies = []
+    ipaddrs = None
+    for p in proxies_chain():
+        temp_proxies.append(p)
+    while success == False:
+        for p in temp_proxies:
             proxy = {
                 "https": p
             }
@@ -37,17 +52,26 @@ def cloud_grap(url,target):
                 "sort":"RELEVANCE",
                 "per_page":"25",
                 "virtual_hosts":"EXCLUDE",
-                "q":url
+                "q":target
             }
             try:
-                res = requests.get(url,params=params,timeout=4,proxies=proxy)
-                if parse_ip(res.text) is not None:
-                    print(parse_ip(res.text))
-                if parse_sub_and_domain(res.text) is not None:
+                res = requests.get(url,params=params,timeout=5,proxies=proxy)
+                if parse_ip(res.text) is not None and len(parse_ip(res.text)) > 0:
+                    print("%sSUCCESS PARSE:%s"%(green,end))
+                    for ipp in parse_ip(res.text):
+                        print(ipp)
                     success = True
-                    print(parse_sub_and_domain(res.text))
                     break
             except Exception as error:
-                print("%sProxy FAIL: %s%s"%(red,p,end))
+                print("%sPROXY FAIL: %s%s"%(red,error,end))
+    return ipaddrs
 
-# cloud_grap(URL,"")
+def get_ips_data(ips):
+    for ip in ips:
+        for proxy in proxies_chain():
+            try:
+                print(wide_scan(None,ip,proxy))
+            except Exception as error:
+                print("%sPROXY FAIL: %s%s"%(red,p,end))
+
+cloud_grap(URL,"")
