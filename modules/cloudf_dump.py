3c7d5650103361f209e2936f23327f302a432277
Anouar Ben Saad <bensaad.tig@gmail.com>
2022-03-20 00:40:37 +0100

    make class.

--- modules/cloudf_dump.py
+++ modules/cloudf_dump.py
@@ -2,8 +2,13 @@ import requests
 import json
 import re
 import sys
+
 from regex import CF_PARSE_SUB_AND_DOMAIN,CF_IP
 from gen_proxies import proxies_chain
+
+if sys.version_info < (3, 0):
+    raise Exception("This program requires Python 3.0 or greater")
+
 end = '\033[1;0m'
 red = '\033[1;91m'
 green = '\033[1;92m'
@@ -11,67 +16,139 @@ f = open('../db/vulners.json')
 DB_LOAD = json.load(f)
 URL = DB_LOAD["base"]["cloudFlare"]["url"]+DB_LOAD["base"]["cloudFlare"]["endpoint"]
 SCANNER = DB_LOAD["base"]["cloudFlare"]["url"]+DB_LOAD["base"]["cloudFlare"]["scanner"]
-def parse_sub_and_domain(data):
-    matched = re.findall(CF_PARSE_SUB_AND_DOMAIN,data)
-    try:
-        return matched
-    except Exception as err:
-        return None
 
-def wide_scan(target,ip,proxy):
-    params = {
-        "resource": "hosts",
-        "sort": "RELEVANCE",
-        "per_page": "25",
-        "virtual_hosts":"EXCLUDE",
-        "q": target
-    }
-    res = requests.get(SCANNER+ip,params=params,proxies=proxy)
-    return res.text
+class CloudDumpException(Exception):
+    '''
+    Custom CloudDump exception class
+    '''
+    def __init__(self, msg="CloudDump exception encountered.", dump=None):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
 
-def parse_ip(data):
-    matched = re.findall(CF_IP,data)
-    try:
-        return matched
-    except Exception as err:
-        return None
+class CloudDump:
+    
+    def __init__(self,
+                url,proxies=None,
+                scanner=None):
 
-def cloud_grap(url,target):
-    success = False
-    temp_proxies = []
-    ipaddrs = None
-    for p in proxies_chain():
-        temp_proxies.append(p)
-    while success == False:
-        for p in temp_proxies:
-            proxy = {
-                "https": p
-            }
+        '''
+        Executes censys analysis and bypass the rate limit using free proxies
+        crawled from free-proxy-list
+
+        :url: censys search url.
+        :proxies: list of proxies crawled
+        :scanner: url when recieve cloudflare ipaddresses and search for adresses.
+        '''
+
+        self._url = url
+        self._proxies = None
+        self._scanner = None
+
+    def _set_scanner_(self, scanner):
+        '''
+        set scanner property url for a specific target.
+        '''
+        
+        self._scanner = scanner
+
+    def _set_proxies_(self,proxies):
+        '''
+        set proxies property crawled from free-proxy-list
+        '''
+
+        self._proxies = proxies
+
+    def _graps_(self,target):
+        '''
+        This method is used to make a crawling-loadbalance
+        
+        so we can receive content from censys and get the addresses which 
+        can be bypassed the rate-limit
+        '''
+
+        success = False
+        temp_proxies = []
+        ipaddrs = []
+        # set proxies to array.
+        [temp_proxies.append(proxy) for proxy in self._proxies]
+        # repeating requests until getting the data
+        while success == False:
+            for p in temp_proxies:
+                proxy = {
+                    "https": p
+                }
+                params = {
+                    "resource":"hosts",
+                    "sort":"RELEVANCE",
+                    "per_page":"25",
+                    "virtual_hosts":"EXCLUDE",
+                    "q":"elissabet.tn"
+                }
+                try:
+                    res = requests.get(self._url,params=params,timeout=5,proxies=proxy)
+                    # check ip addresses found or not.
+                    if self.parse_ip(res.text) is not None and len(self.parse_ip(res.text)) > 0:
+                        [ipaddrs.append(ipp) for ipp in self.parse_ip(res.text)]
+                        success = True
+                        break
+
+                except Exception as error:
+                    print(error)
+        print("ipaddresses_",ipaddrs)
+        return ipaddrs
+
+    def parse_domains(self,data):
+        '''
+        parse subdomains from specefic url.
+        '''
+
+        matched = re.findall(CF_PARSE_SUB_AND_DOMAIN,data)
+        try:
+            return matched
+        except Exception as err:
+            return None
+
+    def wide_scan(self,ip):
+        '''
+        search data from ip.
+        :ip: the target ip crawled.
+        '''
+
+        ports = []
+        comp = {}
+        temp_proxies = [] # array proxies.
+        success = False
+        while not success:
+            [temp_proxies.append(proxy) for proxy in self._proxies]
             params = {
-                "resource":"hosts",
-                "sort":"RELEVANCE",
-                "per_page":"25",
+                "resource": "hosts",
+                "sort": "RELEVANCE",
+                "per_page": "25",
                 "virtual_hosts":"EXCLUDE",
-                "q":target
+                "q": "elissabet.tn"
             }
             try:
-                res = requests.get(url,params=params,timeout=5,proxies=proxy)
-                if parse_ip(res.text) is not None and len(parse_ip(res.text)) > 0:
-                    print("%sSUCCESS PARSE:%s"%(green,end))
-                    for ipp in parse_ip(res.text):
-                        print(ipp)
-                    success = True
-                    break
-            except Exception as error:
-                print("%sPROXY FAIL: %s%s"%(red,error,end))
-    return ipaddrs
+                res = requests.get(self.SCANNER+ip,params=params,proxies=proxy)
+                # parse data..
+            except Exception as err:
+                print(err)
 
-def get_ips_data(ips):
-    for ip in ips:
-        for proxy in proxies_chain():
-            try:
-                print(wide_scan(None,ip,proxy))
-            except Exception as error:
-                print("%sPROXY FAIL: %s%s"%(red,p,end))
+    def parse_ip(self,data):
+        '''
+        parse ip addresses, from data content.
+        '''
 
-cloud_grap(URL,"")
+        matched = re.findall(CF_IP,data)
+        try:
+            return matched
+        except Exception as err:
+            return None
+
+
+# cloud_grap(URL,"")
+obj = CloudDump(URL)
+print(obj._url)
+print(proxies_chain())
+obj._set_proxies_(proxies_chain())
+ips = obj._graps_("")
